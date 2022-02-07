4671121a96297c379cf071ca9cc20cc6713f7d54
Prateek Yadav <86231501+hiPrateekYadav@users.noreply.github.com>
2022-02-07 21:47:52 +0530

    modified

--- website/urls.py
+++ website/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.views.static import serve
-from django.conf.urls import url
-from django.conf import settings
+# from django.conf.urls import url
+# from django.conf import settings
 
 admin.site.site_header="Gamer's Destiny"
 admin.site.site_title="GD Admin Portal"
@@ -26,6 +26,6 @@ admin.site.index_title="Welcome to Destiny of Gamers"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('games.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':    settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}) 
-]
+#     url(r'^media/(?P<path>.*)$', serve,{'document_root':    settings.MEDIA_ROOT}), 
+#     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}) 
+# ]
