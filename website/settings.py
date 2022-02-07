200fa8390d6c700a4f6f1b0980b75999ee03d74b
Prateek Yadav <86231501+hiPrateekYadav@users.noreply.github.com>
2022-02-07 21:46:24 +0530

    modified

--- website/settings.py
+++ website/settings.py
@@ -139,4 +139,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'khulkehasso@gmail.com'
-EMAIL_HOST_PASSWORD = 'raopyadav'
+# EMAIL_HOST_PASSWORD = 'password removed'
